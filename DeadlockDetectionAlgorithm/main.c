a44c52e94c5617e24a5c8861ad6bb37503f6613c
James Hall <greatwolf3d@gmail.com>
2015-09-17 19:26:36 -0700

    Added some comments and final changes

--- DeadlockDetectionAlgorithm/main.c
+++ DeadlockDetectionAlgorithm/main.c
@@ -8,135 +8,139 @@
 //  Should be able to simulate Deadlock Detection and Recovery.
 
 #include <stdio.h>
-//#include <conio.h>// TODO: come back to this and figure out what it's for
-
 /* Our Data Structure */
-int max[5][5];// max [n][m] n = process, m = resources 
-int alloc[5][5];
-int need[5][5];
-int avail[5];
+int available[10];// m number of resource types
+int allocation[10][10];// Matrix of nxm, n = #resources m= #resourcetypes
+int request[10][10];// max [n][m] n = resource, m = resourcesTypes
+int need[10][10];
 
-int n, r;
+int numberOfProcesses, numberOfResources;// process, resource
+
+// it's C so declare your methods at the top.
 void input();// Asks for all inputs
 void show();// Shows all outpus
-void cal();// Does all the heavy lifting.
+void compute();// Does all the heavy lifting.
 
+// In main we put all of our functions together so they
 int main()
 {
-
-    printf("------------------Deadlock Detection Algorithm ---------------------------\n");
+    printf("Deadlock Detection Algorithm\n");
     input();
     show();
-    calculate();
-//    getch(); working on this
+    compute();
     return 0;
 }
-
+// I could put in some validation, but it's more important to show the concepts
 void input()
 {
-    int i, j;
-    printf("Enter the number of Processes\t ");
-    scanf("%d", &n);
-    printf("Enter the number of Resource Instances\t");
-    scanf("%d",&r);
-    printf("Enter the Max Matrix\n");
-    for( i = 0; i < n; i++)
+    int i, j;// counters
+    printf("Enter the number of Processes:\t");// single integer, no limits
+    scanf("%d", &numberOfProcesses);
+    printf("Enter the number of Resource Instances:\t");// single integer, no limits
+    scanf("%d",&numberOfResources);
+    printf("Enter the Request Matrix:\n");
+    for( i = 0; i < numberOfProcesses; i++)
     {
-        for(j = 0; j < r; j++)
+        printf("process %d:\n", i);
+        for(j = 0; j < numberOfResources; j++)
         {
-            scanf("%d", &max[i][j]);
+            scanf("%d", &request[i][j]);
         }
     }
-    printf("Enter the Allocation Maxtrix\n ");
-    for(i = 0; i < n; i++)
+    printf("Enter the Allocation Maxtrix:\n");
+    for(i = 0; i < numberOfProcesses; i++)
     {
-        for (j = 0; j < r; j++)
+        printf("process %d:\n", i);
+        for (j = 0; j < numberOfResources; j++)
         {
-            scanf("%d", &alloc[i][j]);
+            scanf("%d", &allocation[i][j]);
         }
     }
-    printf("Enter the available Resources\n ");
-    for(j = 0; j < r; j++)
+    printf("Enter the available Resources:\n");
+    for(j = 0; j < numberOfResources; j++)
     {
-        scanf("%d", &avail[j]);
+        scanf("%d", &available[j]);
     }
 }
+
 void show()
 {
     int i, j;
-    printf("Process\t Allocation\t Max\t Available\t ");
-    for(i = 0; i < n; i++)
+    printf("Process\t Allocation\t Request\t Available\t ");
+    
+    for(i = 0; i < numberOfProcesses; i++)
     {
-        printf("\nP%d\t ", i+1);
-        for(j = 0; j < r; j++)
+        printf("\nP%d\t", i+1);
+        
+        for(j = 0; j < numberOfResources; j++)
         {
-            printf("%d ", alloc[i][j]);
+            printf("%d ", allocation[i][j]);
         }
         printf("\t ");
-        for(j = 0; j < r; j++)
+        
+        for(j = 0; j < numberOfResources; j++)
         {
-            printf("%d ", max[i][j]);
+            printf("%d ", request[i][j]);
         }
         printf("\t");
+        
         if (i == 0) {
-            for (j = 0; j < r; j++)
+            for (j = 0; j < numberOfResources; j++)
             {
-                printf("%d", avail[j]);
+                printf("%d ", available[j]);
             }
         }
     }
 }
 
-void calculate()
+void compute()
 {
-    int finish[5], temp, need[5][5], flag = 1, k, c1 = 0;
-    int dead[5];
-    int safe[5];
+    int finish[10], need[10][10], flag = 1, k;// for the integer arrays, we are declared the maximum space we could take up.
+    int dead[10];
     int i, j;
-    for( i = 0; i < n; i ++ )
+    for( i = 0; i < numberOfProcesses; i ++ )
     {
         finish[i]=0;
     }
-    //find need matrix
-    for(i = 0; i < n; i++)
+    for(i = 0; i < numberOfProcesses; i++)
     {
-        for(j = 0; j < r; j++)
+        for(j = 0; j < numberOfResources; j++)
         {
-            need[i][j] = max[i][j] - alloc[i][j];
+            need[i][j] = request[i][j] - allocation[i][j];
         }
     }
     while(flag)
     {
         flag = 0;
-        for (i = 0; i < n; i++)
+        for (i = 0; i < numberOfProcesses; i++)
         {
             int c = 0;
-            for( j = 0; j < r; j++)
+            for( j = 0; j < numberOfResources; j++)
             {
-                if ( ( finish[i]==0 ) && ( need[i][j] <= avail[j] ) )
+                if ( ( finish[i] == 0 ) && ( need[i][j] <= available[j] ) )
                 {
                     c++;
-                    if(c==r)
+                    if ( c == numberOfResources )
                     {
-                        for(k=0;k<r;k++)
+                        for(k = 0; k < numberOfResources; k++)
                         {
-                            avail[k]+= alloc[i][j];
+                            available[k] += allocation[i][j];// slightly different from the algorithm in our paper.
                             finish[i] = 1;
                             flag = 1;
                         }
-                        printf("\nP%d", i);
                         if (finish[i]==1)
                         {
-                            i = n;
+                            i = numberOfProcesses;
                         }
                     }
                 }
             }
         }
     }
+    // go through and find the deadlocked processes.
     j = 0;
     flag = 0;
-    for(i = 0; i < n; i++)
+    for(i = 0; i < numberOfProcesses; i++)
     {
         if(finish[i] == 0)
         {
@@ -148,11 +152,11 @@ void calculate()
     if(flag == 1)
     {
         printf("\n\nSystem is in Deadlock and the Deadlock process are \n");
-        for(i = 0; i < n; i++)
+        for(i = 0; i < numberOfProcesses; i++)
         {
-            printf("P%d\t", dead[i]);
+            printf("Process#%d\t", dead[i]);
         }
     }else{
-        printf("\n No Deadlock Occur");
+        printf("\n No Deadlock");
     }
 }
